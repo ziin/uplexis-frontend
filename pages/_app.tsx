695bb6b23410db8c7bc623ea58f8f6e681ac1f86
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2021-11-20 00:14:39 -0400

    feat: add fontawesome necessary icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,35 @@
 import type { AppProps } from 'next/app'
 import { globalStyles } from '../stitches.config'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  faBan,
+  faBriefcase,
+  faChevronLeft,
+  faGavel,
+  faGem,
+  faGlobe,
+  faGlobeAmericas,
+  faLandmark,
+  faMale,
+  faPiggyBank,
+  faTree,
+} from '@fortawesome/free-solid-svg-icons'
+
+// Adding the icons to the library, for them to be available globally.
+// It also reduces the bundle size.
+library.add(
+  faBriefcase,
+  faLandmark,
+  faTree,
+  faGavel,
+  faBan,
+  faGlobeAmericas,
+  faGem,
+  faMale,
+  faPiggyBank,
+  faGlobe,
+  faChevronLeft
+)
 
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles()
