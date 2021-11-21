956931e2eb5613d50116b4c1d19fa80332f5652a
Alisson L. Schwanz <alissonludtkeschwanz@gmail.com>
2021-11-21 17:17:14 -0400

    feat: style CategoryBar, add dragscroll to it

--- components/CategoryBar.tsx
+++ components/CategoryBar.tsx
@@ -1,29 +1,118 @@
 import { Category } from '../data/categories'
+import { styled } from '../stitches.config'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import DragScroll from 'react-indiana-drag-scroll'
 
 const CategoryBar = ({ categories, selected, onSelect }: Props) => {
   return (
-    <nav>
-      <ul>
-        <li
-          key="all"
-          onClick={() => onSelect(null)}
-          style={{ fontWeight: selected === null ? 700 : 400 }}
-        >
+    <Nav>
+      <List>
+        <Item key={-1} onClick={() => onSelect(null)} selected={!selected}>
+          <Icon icon="globe" />
           <span>Todos</span>
-        </li>
+        </Item>
         {categories.map((category) => (
-          <li
+          <Item
             key={category.id}
             onClick={() => onSelect(category)}
-            style={{ fontWeight: selected?.id === category.id ? 700 : 400 }}
+            selected={selected?.id === category.id}
           >
+            <Icon icon={category.icon as IconProp} />
             <span>{category.name}</span>
-          </li>
+          </Item>
         ))}
-      </ul>
-    </nav>
+      </List>
+    </Nav>
   )
 }
+
+const Nav = styled('nav', { my: '$12' })
+
+const List = styled(DragScroll, {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  width: '100%',
+  height: '$64',
+  borderRadius: '$sm',
+
+  '@tablet': {
+    height: '$84',
+  },
+})
+
+const Item = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  minWidth: '$112',
+  height: '100%',
+  backgroundColor: '$gray100',
+  cursor: 'pointer',
+  fontSize: '$13',
+  gap: '$8',
+  px: '$8',
+  textAlign: 'center',
+  userSelect: 'none',
+  whiteSpace: 'nowrap',
+  border: '1px solid $gray200',
+  borderRight: 'none',
+
+  '&:first-of-type': {
+    borderTopLeftRadius: '$sm',
+    borderBottomLeftRadius: '$sm',
+  },
+  '&:last-of-type': {
+    borderBottomRightRadius: '$sm',
+    borderTopRightRadius: '$sm',
+  },
+
+  variants: {
+    selected: {
+      false: {
+        '&:hover': {
+          backgroundColor: '$orangeLight',
+          borderColor: '$orangeLight',
+          color: '$gray100',
+
+          '& + div': {
+            borderLeft: '1px solid transparent',
+          },
+        },
+
+        '&:last-of-type': {
+          borderRight: '1px solid $gray200',
+
+          '&:hover': {
+            borderColor: '$orangeLight',
+          },
+        },
+      },
+      true: {
+        backgroundColor: '$orange',
+        borderColor: '$orange',
+        color: '$white',
+        boxShadow: '0px 0px 8px #FF6F01',
+        zIndex: '9999',
+
+        '& + div': {
+          borderLeft: '1px solid transparent',
+        },
+
+        '&:last-of-type': {
+          borderRight: '1px solid $orange',
+        },
+      },
+    },
+  },
+})
+
+const Icon = styled(FontAwesomeIcon, {
+  fontSize: '$16',
+})
 
 type Props = {
   categories: Category[]
